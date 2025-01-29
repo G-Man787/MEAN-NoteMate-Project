c04d05704cff9dcf683d77ff1e7160842c2099f1
Getachew Akalu <getachewakalu@Getachews-iMac.local>
2025-01-28 18:55:14 -0800

    MEAN stack project to create note to be saved for a reference

--- notes-api/helpers/db-connect.js
+++ notes-api/helpers/db-connect.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/NoteMate', { useNewUrlParser: true }).then(() => {
+mongoose.connect('mongodb+srv://<db_getachew>:<db_zagkih-beHdic-wyrva8>@cluster0.pe44h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true }).then(() => {
     console.log('Connected to MongoDB successfully');
 }).catch(e => {
     console.log('Error while attempting to connect to MongoDB');
